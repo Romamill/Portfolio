0c1db4da902fb771f85ae57f1ae744f79792a9a6
Romamill <122994080+Romamill@users.noreply.github.com>
2024-08-09 13:15:26 +0300

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,49 @@
 /*=============== SERVICES MODAL ===============*/
+const modal = document.querySelectorAll(`.services__modal`),
+    modalButton = document.querySelectorAll(`.services__button`),
+    modalClose = document.querySelectorAll(`.services__modal-close`)
 
+let activeModal = (modalClick) => {
+    modal[modalClick].classList.add(`active-modal`)
+}   
+
+modalButton.forEach((modalButton, i) => {
+    modalButton.addEventListener(`click`, () => {
+        activeModal(i)
+    })
+})
+
+modalClose.forEach((modalClose) => {
+    modalClose.addEventListener(`click`, () => {
+        closeModal()
+    })
+})
+
+
+let closeModal = () => {
+    modal.forEach((modalRemove) => {
+        modalRemove.classList.remove(`active-modal`)
+    })
+}
+
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        closeModal()
+    }
+})
+
+
+modal.forEach((modalItem) => {
+    modalItem.addEventListener('click', (event) => {
+        if (event.target === modalItem) {
+            closeModal()
+        }
+    })
+})
 
 /*=============== SWIPER TESTIMONIAL ===============*/
+
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
